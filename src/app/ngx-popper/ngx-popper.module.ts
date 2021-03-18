b7cdd105d4c1a9ba3819fa2e47185e5d614f5fe9
Kristof Marien <krmarien@gmail.com>
2021-03-18 06:09:07 +0100

    Add popper demo

--- src/app/ngx-popper/ngx-popper.module.ts
+++ src/app/ngx-popper/ngx-popper.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { NgxPopperComponent } from './ngx-popper.component';
+import { TourNgxPopperModule } from '@ngx-tour/ngx-popper';
+import { NgxPopperjsModule } from 'ngx-popperjs';
 import { DocsComponent } from './docs.component';
+import { NgxPopperComponent } from './ngx-popper.component';
 import { OtherRouteComponent } from './other-route.component';
 
 @NgModule({
   declarations: [NgxPopperComponent, DocsComponent, OtherRouteComponent],
   imports: [
     CommonModule,
+    NgxPopperjsModule,
     RouterModule.forChild([{
       component: NgxPopperComponent,
       path: '',
@@ -21,6 +23,7 @@ import { OtherRouteComponent } from './other-route.component';
         path: 'other',
       }]
     }]),
+    TourNgxPopperModule.forRoot(),
   ],
 })
 export class NgxPopperModule { }
